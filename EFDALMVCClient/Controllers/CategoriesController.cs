8749b18a992988f8fe41746779a2684d1ca1cf6f
gauravshahare1 <121213204+gauravshahare1@users.noreply.github.com>
2024-08-09 09:17:06 +0530

    all

---
+++ EFDALMVCClient/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using EFDAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFDALMVCClient.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        FlipkartContext _db;
+
+        public CategoriesController(FlipkartContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var catogories = _db.Categories.ToList();
+            return View(catogories);
+        }
+    }
+}
